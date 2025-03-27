64773588b9724aeb50e7a24ef80a0818a3a1ed3c
Al-Eduardo <st10445832@vcconnect.edu.za>
2025-03-27 13:13:13 +0200

    Add project files.

---
+++ MenuService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10445832_PROG6221_Part1
+{
+    class MenuService
+    {
+    }
+}
+////////////////////////////////////////////////END OF FILE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10445832_PROG6221_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+////////////////////////////////////////////////END OF FILE\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
