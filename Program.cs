f62b0955f9e521e4ac608f6787fb369b9cd16923
ST10445832-Alex <163132023+ST10445832-Alex@users.noreply.github.com>
2025-04-15 20:48:54 +0200

    Added comments and removed unused using statements

--- Program.cs
+++ Program.cs
@@ -2,20 +2,15 @@
 // ST10445832
 // GR01
 
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ST10445832_PROG6221_Part1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Create instance of menu service
             MenuService menuService = new MenuService();
+            // Run menu service startup tasks
             menuService.RunStartUpSequence();
         }
     }
